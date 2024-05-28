262a6771e864f0892ee99de2a01582ff1f829990
nahuex <ncortesguiet@gmail.com>
2024-05-28 20:28:21 -0300

    Pasar la IP del servidor como argumento
    Permitir pasar la IP del servidor como argumento

--- cliente.py
+++ cliente.py
@@ -7,3 +7,7 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = 12345
 BUFFER_SIZE = 1024
 MESSAGE_DELIMITER = b'\n'
+
+# Permitir pasar la IP del servidor como argumento
+if len(sys.argv) >= 2:
+    TCP_IP = sys.argv[1]
