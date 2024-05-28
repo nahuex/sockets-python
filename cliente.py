0ec108b001e75fce1fa42e94400512400adb740f
nahuex <ncortesguiet@gmail.com>
2024-05-28 20:30:15 -0300

    Funcion recibir_mensajes
    Recibe mensajes del servidor y los imprime

--- cliente.py
+++ cliente.py
@@ -11,3 +11,28 @@ MESSAGE_DELIMITER = b'\n'
 # Permitir pasar la IP del servidor como argumento
 if len(sys.argv) >= 2:
     TCP_IP = sys.argv[1]
+
+def recibir_mensajes(sock):
+    """Recibe mensajes del servidor y los imprime"""
+    while True:
+        try:
+            data = bytearray()
+            while True:
+                recvd = sock.recv(BUFFER_SIZE)
+                if not recvd:
+                    break
+                data += recvd
+                if MESSAGE_DELIMITER in recvd:
+                    msg = data.rstrip(MESSAGE_DELIMITER).decode('utf-8')
+                    print(f"[CLIENTE] Mensaje Recibido: {msg}")
+                    if msg.lower() == "logout":
+                        print("[CLIENTE] Desconectando del Servidor")
+                        sock.close()
+                        return
+                    data.clear()
+        except ConnectionError:
+            print("[CLIENTE] Error de conexión")
+            break
+        except Exception as e:
+            print(f"[CLIENTE] Error inesperado: {e}")
+            break    
