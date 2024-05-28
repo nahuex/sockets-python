cdb43e25fd8f4b0a86892291493e61f4c6fc63fb
nahuex <ncortesguiet@gmail.com>
2024-05-28 20:26:18 -0300

    Configuración de la dirección IP y puerto del servidor
    Configuración de la dirección IP y puerto del servidor

--- cliente.py
+++ cliente.py
@@ -1,3 +1,9 @@
 import sys
 import socket
 import threading
+
+# Configuración de la dirección IP y puerto del servidor
+TCP_IP = '127.0.0.1'
+TCP_PORT = 12345
+BUFFER_SIZE = 1024
+MESSAGE_DELIMITER = b'\n'
