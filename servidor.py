5ae1ed0072a48fca97bbca8fad007e301eb5952c
nahuex <ncortesguiet@gmail.com>
2024-05-28 20:14:57 -0300

    Diccionario para almacenar las conexiones de clientes
    Diccionario para almacenar las conexiones de clientes

--- servidor.py
+++ servidor.py
@@ -6,3 +6,5 @@ TCP_IP = '0.0.0.0'  # Escuchar en todas las interfaces de red disponibles
 TCP_PORT = 12345
 BUFFER_SIZE = 1024
 MESSAGE_DELIMITER = b'\n'
+
+clientes = {}  # Diccionario para almacenar las conexiones de clientes
