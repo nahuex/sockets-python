d2747e636ae17d883091bfd400db18affc104dcf
nahuex <ncortesguiet@gmail.com>
2024-05-28 20:21:40 -0300

    with contacto_cliente

--- servidor.py
+++ servidor.py
@@ -20,3 +20,30 @@ def contacto_cliente(conn, addr):
     print(f"[SERVIDOR] Conectado satisfactoriamente con {addr}")
     clientes[addr] = conn
     print(f"[SERVIDOR] Clientes conectados: {len(clientes)}")
+
+    with conn:
+        data = bytearray()
+        while True:
+            try:
+                received = conn.recv(BUFFER_SIZE)
+                if not received:
+                    break
+                data += received
+                if MESSAGE_DELIMITER in received:
+                    msg = data.rstrip(MESSAGE_DELIMITER).decode('utf-8')
+                    print(f"[SERVIDOR] Mensaje de {addr}: {msg}")
+                    if msg.lower() == "logout":
+                        break
+                    if msg.startswith('#'):
+                        broadcast(data, conn)
+                    else:
+                        conn.sendall(data)  # Eco
+                    data.clear()
+            except ConnectionError:
+                break
+            except Exception as error:
+                print(f"[SERVIDOR] Error: {error}")
+                break
+    print(f"[SERVIDOR] Desconectando {addr}")
+    del clientes[addr]
+    print(f"[SERVIDOR] Clientes conectados: {len(clientes)}")
